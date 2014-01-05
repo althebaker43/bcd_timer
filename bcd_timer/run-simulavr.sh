d633f035b2d49355556ebf13d43dfe1a1c7ff946
Allen Baker <althebaker43@gmail.com>
2014-01-05 10:56:55 -0600

    Fixed OUT instruction not using adjusted IO address. Increased length of simulation to 4 seconds. Simulation passes.

--- bcd_timer/run-simulavr.sh
+++ bcd_timer/run-simulavr.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-simulavr --device atmega328 --cpufrequency 1000000 -c vcd:bcd_timer.traces:bcd_timer.vcd -f bcd_timer.elf -m 40000000
+simulavr --device atmega328 --cpufrequency 1000000 -c vcd:bcd_timer.traces:bcd_timer.vcd -f bcd_timer.elf -m 4000000000
